80fa1e0c313be923e9e468640cab641566a03b78
Rada Bogdan <bogdan.rada@softvision.ro>
2020-01-10 10:20:43 +0200

    properly run bundler on travis - added also ruby 2.2.2 and ruby 2.6.5 to matrix

--- install-bundler.sh
+++ install-bundler.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o pipefail
-set -o nounset
+rails_version="$(basename "${BUNDLE_GEMFILE:="$(pwd)/Gemfile"}" .gemfile | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')"
 
-rails_version="$(echo "$(basename $BUNDLE_GEMFILE .gemfile)" | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')"
+echo "Trying to compare rails ${rails_version:="1"} with 5.2"
 
-echo "Trying to compare rails ${rails_version} with 5.1"
-
-if (( $(echo "${rails_version%.*} >= 5.1" |bc -l) )); then
+if (( $(echo "${rails_version%.*} >= 5.2" |bc -l) )); then
    echo "Installing Bundler v2"
-   gem uninstall -v '< 2.0' -i $(rvm gemdir)@global -ax bundler --force || true
+   gem uninstall -v '< 2.0' -i "$(rvm gemdir)"@global -ax bundler --force || true
    gem install bundler -v '2.0.1'
+   export MY_BUNDLER_VERSION="_2.0.1_"
 else
    echo "Installing Bundler v1"
-   gem uninstall -v '>= 2' -i $(rvm gemdir)@global -ax bundler --force  || true
+   gem uninstall -v '>= 2' -i "$(rvm gemdir)"@global -ax bundler --force  || true
    gem install bundler -v '1.17.3'
+   export MY_BUNDLER_VERSION="_1.17.3_"
 fi
 
 unset rails_version
